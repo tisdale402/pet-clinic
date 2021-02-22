b970826882d85379596573d4e9b842f4996a54a8
Alex Tisdale <Tisdale402@gmail.com>
2021-02-22 12:53:17 -0600

    Version 0.0.6
    Implement Simple CrudRepo

--- pet-clinic-data/src/main/java/tisda/petclinc/services/OwnerService.java
+++ pet-clinic-data/src/main/java/tisda/petclinc/services/OwnerService.java
@@ -4,14 +4,8 @@ import tisda.petclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastname);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/tisda/petclinc/services/VetService.java
+++ pet-clinic-data/src/main/java/tisda/petclinc/services/VetService.java
@@ -2,15 +2,7 @@ package tisda.petclinc.services;
 
 import tisda.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 
 }
