00ea6bc82a3035da61c466fd9ac17fe9d6b799b9
Alex Tisdale <Tisdale402@gmail.com>
2021-02-21 19:46:53 -0600

    Ver 0.0.3
    Setup multi-module structure

--- pet-clinic-web/src/test/java/tisda/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/tisda/petclinic/PetClinicApplicationTests.java
@@ -1,6 +1,9 @@
 package tisda.petclinic;
 
 import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.junit5.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
